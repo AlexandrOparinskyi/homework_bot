0cc072d71a2dcc5c637568f6c828414d37b883ed
alex <opar1297@mail.ru>
2022-05-27 18:45:23 +0300

    Добавлено немного логов

--- homework.py
+++ homework.py
@@ -1,9 +1,17 @@
+import logging
 import os
+import time
 
-from dotenv import load_dotenv
 import requests
 import telegram
-import time
+from dotenv import load_dotenv
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    format='%(asctime)s, %(levelname)s, %(message)s',
+    filemode='a'
+)
 
 load_dotenv()
 
@@ -15,7 +23,6 @@ RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
-
 HOMEWORK_STATUSES = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
@@ -26,17 +33,21 @@ HOMEWORK_STATUSES = {
 def send_message(bot, message):
     """Отправка сообщения"""
     bot.send_message(TELEGRAM_CHAT_ID, message)
+    logging.info(f'Отправлено сообщение: {message}')
 
 
 def get_api_answer(current_timestamp):
     """Запрос к эндпоинту API"""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-    return requests.get(
+    response = requests.get(
         url=ENDPOINT,
         headers=HEADERS,
         params=params
-    ).json()
+    )
+    if response.status_code != 200:
+        logging.error('Нет ответа от API')
+    return response.json() or False
 
 
 def check_response(response):
@@ -67,9 +78,14 @@ def main():
     if check_tokens() is True:
         bot = telegram.Bot(token=TELEGRAM_TOKEN)
         current_timestamp = int(time.time())
+    else:
+        logging.critical('Отсутствуют обязательные переменные')
     while True:
         try:
             response = get_api_answer(current_timestamp)
+            if response is False:
+                logging.error('С API что-то не так')
+            print(response)
             homework = check_response(response)
             if len(homework) != 0:
                 message = parse_status(homework)
@@ -78,9 +94,10 @@ def main():
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
             send_message(bot, message)
+            logging.error(f'Сбой при отправке сообщения. Ошибка: {error}')
             time.sleep(RETRY_TIME)
         else:
-            print('Работу не проверили')
+            logging.info('Работу не проверили')
             time.sleep(RETRY_TIME)
 
 
