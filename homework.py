04daf7cca251c5096f3949689923f6d77bb98c0d
Александр Опаринский <oparinskyi@yandex.ru>
2022-05-30 11:02:51 +0300

    Check

--- homework.py
+++ homework.py
@@ -40,7 +40,7 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp):
     """Запрос к эндпоинту API."""
     timestamp = current_timestamp or int(time.time())
-    params = {'from_date': 0}
+    params = {'from_date': timestamp}
     response = requests.get(
         url=ENDPOINT,
         headers=HEADERS,
